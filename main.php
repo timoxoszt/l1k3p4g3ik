1e9f4f050fce8b4840b535e0682b5dc7d3923195
VI MINH TIẾN <20520810@gm.uit.edu.vn>
2021-09-23 17:14:29 +0700

    delete php

--- main.php
+++
@@ -1,9 +0,0 @@
-<?php
-	require "fbsdk/src/Facebook/autoload.php";
-	session_start();
-	$fb = new Facebook\Facebook([
-	  'app_id'                => '682359065970471',
-	  'app_secret'            => 'cabd262a1fff484d479a4bea676312ed',
-	  'default_graph_version' => 'v12',
-	]);
-?>
